4d50050c87bda7572395eac91e568400f96ada2f
Yash <goldihr26@gmail.com>
2026-08-13 22:17:35 +0530

    Fix Flask-Mail configuration and sender

--- main.py
+++ main.py
@@ -89,8 +89,10 @@ google = oauth.register(
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')  # Google App Password (16-digit code)
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
 
 mail = Mail(app)
 serializer = URLSafeTimedSerializer(app.secret_key)
