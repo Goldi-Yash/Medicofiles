11cbb87df55c2dc2c07f1d8d3f1ea24fa6e4089f
Yash <goldihr26@gmail.com>
2026-08-13 22:25:23 +0530

    Switch SMTP to SSL Port 465

--- main.py
+++ main.py
@@ -87,9 +87,9 @@ google = oauth.register(
 
 # Flask-Mail Configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')  # Google App Password (16-digit code)
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
